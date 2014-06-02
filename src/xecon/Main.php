55c1416e61712ceafb3bf9eb7f972c8428ee98ed
PEMapModder <pemapmodder1970@gmail.com>
2014-06-03 00:26:13 +0800

    Added basic things. Hope my structuring is good enough.

--- src/xecon/Main.php
+++ src/xecon/Main.php
@@ -4,6 +4,19 @@ namespace xecon;
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use xecon\utils\FolderDatabase;
 
 class Main extends PluginBase implements Listener{
+	/** @var  string directory where economic entity information is stored` */
+	private $edir;
+	/** @var  FolderDatabase $db */
+	private $db;
+	public function onEnable(){
+		$this->mkdirs();
+		$this->db = new FolderDatabase($this->edir);
+	}
+	private function mkdirs(){
+		@mkdir($this->getDataFolder());
+		@mkdir($this->edir = $this->getDataFolder()."entities database/");
+	}
 }
---
+++ src/xecon/entity/Entity.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace xecon\entity;
+
+
+class Entity {
+
+} 
